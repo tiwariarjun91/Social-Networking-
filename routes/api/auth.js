b57b9eef333baae474ef47ccddf3d6a6713041b3
tiwariarjun91 <tiwariarjun91@gmail.com>
2020-05-27 14:02:18 +0530

    auth middleware and jwt veryfing

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+const User = require('../../models/User');
 
 // @route   GET /api/auth
 // @desc    tets route
 // @access  public
-router.get('/', (req, res) => res.send('auth routes'));
+router.get('/', auth,async (req, res) => {
+
+    try{
+        const user = await User.findById(req.user.id).select('-password');
+        res.json(user);
+    }catch(err){
+        console.error(err.message);
+        res.status(500).send('Server error');
+
+ }
+});
 
 
 module.exports = router;
