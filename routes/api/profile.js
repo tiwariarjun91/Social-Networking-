ee70896a69ea73d93b12dd32d899cf04e81605ac
tiwariarjun91 <tiwariarjun91@gmail.com>
2020-09-23 01:01:21 +0530

    Fetching github repos

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const request = require('request');
+const config = require('config');
 const auth = require('../../middleware/auth');
 const {check, validationResult} = require('express-validator/check');
 
@@ -361,16 +363,40 @@ router.delete('/education/:edu_id', auth , async (req, res) => {
 } catch (err ){
   console.error(err.message);
   res.status(500).send('Server Message');
-
-
-  
 }
+});
 
+// @route   GET /api/profile/github/: username
+// @desc    Get user repos
+// @access  Public
 
+router.get('/github/:username', (req,res) => {
 
+  try {
+    const options = {
+      uri: `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc&client_id=${config.get('githubClientId')}&client_secret=${config.get('githubSecret')}`,
+      method : 'GET',
+      headers : {'user-agent' : 'node.js'} };
 
+      request(options, (error, response, body) => {
+
+        if(error) error.console(error);
+
+        if(response.statusCode !== 200) {res.status(404).json({msg : 'No github profile found'});
+      }
+      res.json(JSON.parse(body));
+      });
+    }
+    
+   catch (err) {
+     console.error(err.message);
+     res.status(500).send('Server Error');
+    
+  }
 
 });
+
+
 
 
 module.exports = router;
